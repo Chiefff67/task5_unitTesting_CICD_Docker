1d2e0f81c9642370f736c7b7ebed6e41b94d3c2c
Chiefff67 <bocahrkr@gmail.com>
2024-11-05 17:50:22 +0800

    fix bug

--- src/config/database.ts
+++ src/config/database.ts
@@ -15,10 +15,13 @@ const connectDB = async (): Promise<void> => {
     } as ConnectOptions);
 
     console.log("Database connected successfully");
+    console.log("MONGO_URI:", process.env.MONGO_URI);
   } catch (error) {
     console.error("Database connection failed:", error);
     process.exit(1);
   }
 };
+
+
 
 export default connectDB;
