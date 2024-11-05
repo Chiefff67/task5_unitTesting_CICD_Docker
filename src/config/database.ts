788936683a5dcb9d59e049881e884fcb65d90e12
Chiefff67 <bocahrkr@gmail.com>
2024-11-05 22:50:28 +0800

    fix database

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,10 +2,13 @@ import mongoose, { ConnectOptions } from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
+// console.log("NODE_ENV:", process.env.NODE_ENV);
+// console.log("MONGO_URI:", process.env.MONGO_URI);
+// console.log("JWT_SECRET:", process.env.JWT_SECRET);
 
 const connectDB = async (): Promise<void> => {
   try {
-    const uri = process.env.MONGO_URI as string;
+    const uri = process.env.MONGO_URI;
     if (!uri) {
       throw new Error("MONGO_URI not defined in .env file");
     }
@@ -15,7 +18,7 @@ const connectDB = async (): Promise<void> => {
     } as ConnectOptions);
 
     console.log("Database connected successfully");
-    console.log("MONGO_URI:", process.env.MONGO_URI);
+    // console.log("MONGO_URI:", process.env.MONGO_URI);
   } catch (error) {
     console.error("Database connection failed:", error);
     process.exit(1);
